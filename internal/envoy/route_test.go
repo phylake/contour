689b0c736180f4b12238a59d18b0c4a76c338f9c
Laurent Rouquette <lrouquet@adobe.com>
2020-01-17 09:27:51 -0800

    moved TestAdobePerFilterConfig into its own file

--- internal/envoy/route_test.go
+++ internal/envoy/route_test.go
@@ -20,7 +20,6 @@ import (
 	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	envoy_api_v2_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	envoy_api_v2_route "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
-	_struct "github.com/golang/protobuf/ptypes/struct"
 	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/projectcontour/contour/internal/assert"
 	"github.com/projectcontour/contour/internal/dag"
@@ -754,56 +753,3 @@ func TestRouteMatch(t *testing.T) {
 }
 
 func virtualhosts(v ...*envoy_api_v2_route.VirtualHost) []*envoy_api_v2_route.VirtualHost { return v }
-
-func TestPerFilterConfig(t *testing.T) {
-	msi := map[string]interface{}{
-		"filter": map[string]interface{}{
-			"map": map[string]interface{}{
-				"float": 9.0,
-				"bool":  true,
-			},
-			"list": []interface{}{
-				"string",
-			},
-		},
-	}
-	got := make(map[string]*_struct.Struct)
-	for k, v := range msi {
-		s := new(_struct.Struct)
-		got[k] = s
-		recurseIface(s, v)
-	}
-	want := map[string]*_struct.Struct{
-		"filter": {
-			Fields: map[string]*_struct.Value{
-				"map": {
-					Kind: &_struct.Value_StructValue{
-						&_struct.Struct{
-							Fields: map[string]*_struct.Value{
-								"float": {
-									Kind: &_struct.Value_NumberValue{9.0},
-								},
-								"bool": {
-									Kind: &_struct.Value_BoolValue{true},
-								},
-							},
-						},
-					},
-				},
-				"list": {
-					Kind: &_struct.Value_ListValue{
-						&_struct.ListValue{
-							Values: []*_struct.Value{
-								{
-									Kind: &_struct.Value_StringValue{"string"},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	assert.Equal(t, want, got)
-}
